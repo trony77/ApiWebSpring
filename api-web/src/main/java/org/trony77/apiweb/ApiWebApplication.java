50785162fb3d034059f1969592a75b86931905ce
trony77 <trony77@gmail.com>
2020-02-03 13:23:23 +0100

    first commit for initialize project

---
+++ api-web/src/main/java/org/trony77/apiweb/ApiWebApplication.java
@@ -0,0 +1,13 @@
+package org.trony77.apiweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiWebApplication.class, args);
+	}
+
+}
---
+++ api-web/src/test/java/org/trony77/apiweb/ApiWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.trony77.apiweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
